be2506cf955bd995d70f04483c9fd6be3a53c96a
Vaishnao Vaidya <vaishnaovaidya2001@gmail.com>
2023-11-12 13:45:56 +0530

    first commit

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,9 +2,9 @@
 import React from 'react'
 
 export default function Alert(props) {
-  // const capitalize = (word) => {
-  //   const lower = word.toLowerCase();
-  //   return lower.charAt(0).toUpperCase() + lower.slice(1);
+//   const capitalize = (word) => {
+//     const lower = word.toLowerCase();
+//     return lower.charAt(0).toUpperCase() + lower.slice(1);
 // }
   return (
     <div style = {{height:"50px"}}>
